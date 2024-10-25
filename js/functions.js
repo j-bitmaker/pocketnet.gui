8eaf6bac3a1645c7ce748e19d4f199bd256e0478
j-bitmaker <Alexandr@MacBook-Pro-2.local>
2024-10-25 19:50:24 +0300

    refresh after change; init macos

--- js/functions.js
+++ js/functions.js
@@ -10708,3 +10708,49 @@ function isInt(n) {
 function randone() {
     return (Math.round(Math.random()) == 1);
 }
+
+function compareObjects(obj1, obj2) {
+    // Check if both objects are null or undefined
+    if (obj1 === null || obj1 === undefined || obj2 === null || obj2 === undefined) {
+      return obj1 === obj2;
+    }
+  
+    // Check if both objects are of the same type
+    if (typeof obj1 !== typeof obj2) {
+      return false;
+    }
+  
+    // Check if both objects are arrays
+    if (Array.isArray(obj1) && Array.isArray(obj2)) {
+      if (obj1.length !== obj2.length) {
+        return false;
+      }
+      for (let i = 0; i < obj1.length; i++) {
+        if (!compareObjects(obj1[i], obj2[i])) {
+          return false;
+        }
+      }
+      return true;
+    }
+  
+    // Check if both objects are objects
+    if (typeof obj1 === 'object' && typeof obj2 === 'object') {
+      const keys1 = Object.keys(obj1);
+      const keys2 = Object.keys(obj2);
+      
+      if (keys1.length !== keys2.length) {
+        return false;
+      }
+  
+      for (let key of keys1) {
+        if (!compareObjects(obj1[key], obj2[key])) {
+          return false;
+        }
+      }
+      
+      return true;
+    }
+  
+    // Compare primitive values
+    return obj1 === obj2;
+  }
